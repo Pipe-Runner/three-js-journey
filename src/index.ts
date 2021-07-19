4fe2188b8b368f48df5f78c27df0fec065b0344a
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2021-07-19 21:12:58 +0530

    Added built in controls
    - Made use of built-in controls (Orbit)
    - Enabled damping on it

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,9 @@ import {
   AxesHelper,
   PerspectiveCamera,
   Scene,
-  WebGLRenderer,
-  OrthographicCamera
+  WebGLRenderer
 } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import './main.css';
 
 const canvas = document.createElement('canvas');
@@ -27,11 +27,9 @@ const mesh = new Mesh(
 );
 
 const camera = new PerspectiveCamera(75, dimension.width / dimension.height);
-
-// const aspectRation = dimension.width / dimension.height;
-// const camera = new OrthographicCamera(-1 * aspectRation, 1 * aspectRation, 1, -1, 0.1, 100);
-camera.position.set(0, 0, 4);
 camera.lookAt(mesh.position);
+
+camera.position.set(0, 0, 4);
 
 const axesHelper = new AxesHelper(1000);
 
@@ -46,36 +44,17 @@ const renderer = new WebGLRenderer({
 });
 renderer.setSize(dimension.width, dimension.height);
 
-// Canvas Dimension and control cache
-const rect = canvas.getBoundingClientRect();
 
-const dimensionCache = {
-  height: rect.height,
-  width: rect.width
-};
-
-const controlCache = {
-  p: 0,
-  q: 0
-};
-
-// Adding custom controls
-canvas.addEventListener('mousemove', function (event: MouseEvent) {
-  const x = event.offsetX;
-  const y = event.offsetY;
-
-  controlCache.p = x / dimensionCache.width - 0.5;
-  controlCache.q = y / dimensionCache.height - 0.5;
-
-  // camera.position.setX(controlCache.p * 10);
-  // camera.position.setY(controlCache.q * -10);
-
-  camera.position.setX(2 * Math.sin(controlCache.p * 10 * Math.PI * 0.2));
-  camera.position.setZ(2 * Math.cos(controlCache.p * 10 * Math.PI * 0.2));
-  camera.position.setY(controlCache.q * -10);
-});
+// Adding built-in controls
+const control = new OrbitControls(camera, canvas);
+control.target.x = mesh.position.x;
+control.target.y = mesh.position.y;
+control.target.z = mesh.position.z;
+control.enableDamping = true;
+control.dampingFactor = 0.008;
 
 // initial render
+control.update();
 renderer.render(scene, camera);
 
 let rafId: number;
@@ -86,8 +65,7 @@ function gameLoop(timestamp: number): void {
   time = timestamp;
   const standardizingFactor = dTime * 0.00001;
 
-  // mesh.rotation.set(0, Math.PI * 2 * timestamp * standardizingFactor, 0);
-  camera.lookAt(mesh.position);
+  control.update();
   renderer.render(scene, camera);
 
   rafId = requestAnimationFrame(gameLoop);
