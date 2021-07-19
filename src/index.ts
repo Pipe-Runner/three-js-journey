b00215667b42d243b867f4d737e2882cda0b33f1
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2021-07-19 20:13:46 +0530

    Tried out cameras and custom-controls
    - Tested orthographic and perspective cameras
    - Understood the importance of aspect ratio for orthographic camera
    - Created custom controls from moving the camera

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,8 @@ import {
   AxesHelper,
   PerspectiveCamera,
   Scene,
-  WebGLRenderer
+  WebGLRenderer,
+  OrthographicCamera
 } from 'three';
 import './main.css';
 
@@ -26,7 +27,11 @@ const mesh = new Mesh(
 );
 
 const camera = new PerspectiveCamera(75, dimension.width / dimension.height);
-camera.position.set(0, 0, 3);
+
+// const aspectRation = dimension.width / dimension.height;
+// const camera = new OrthographicCamera(-1 * aspectRation, 1 * aspectRation, 1, -1, 0.1, 100);
+camera.position.set(0, 0, 4);
+camera.lookAt(mesh.position);
 
 const axesHelper = new AxesHelper(1000);
 
@@ -41,4 +46,51 @@ const renderer = new WebGLRenderer({
 });
 renderer.setSize(dimension.width, dimension.height);
 
+// Canvas Dimension and control cache
+const rect = canvas.getBoundingClientRect();
+
+const dimensionCache = {
+  height: rect.height,
+  width: rect.width
+};
+
+const controlCache = {
+  p: 0,
+  q: 0
+};
+
+// Adding custom controls
+canvas.addEventListener('mousemove', function (event: MouseEvent) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+
+  controlCache.p = x / dimensionCache.width - 0.5;
+  controlCache.q = y / dimensionCache.height - 0.5;
+
+  // camera.position.setX(controlCache.p * 10);
+  // camera.position.setY(controlCache.q * -10);
+
+  camera.position.setX(2 * Math.sin(controlCache.p * 10 * Math.PI * 0.2));
+  camera.position.setZ(2 * Math.cos(controlCache.p * 10 * Math.PI * 0.2));
+  camera.position.setY(controlCache.q * -10);
+});
+
+// initial render
 renderer.render(scene, camera);
+
+let rafId: number;
+let time = performance.now();
+
+function gameLoop(timestamp: number): void {
+  const dTime = timestamp - time;
+  time = timestamp;
+  const standardizingFactor = dTime * 0.00001;
+
+  // mesh.rotation.set(0, Math.PI * 2 * timestamp * standardizingFactor, 0);
+  camera.lookAt(mesh.position);
+  renderer.render(scene, camera);
+
+  rafId = requestAnimationFrame(gameLoop);
+}
+
+rafId = requestAnimationFrame(gameLoop);
