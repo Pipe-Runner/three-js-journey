5d30a242ef679bca05e4492cd72a7f799cbbe6fb
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2021-07-24 15:19:08 +0530

    Setting up webpack and texture
    - Setup webpack to directly use static files without absolute path
    - Load an image using as texture using native JS

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,20 @@ import {
   AxesHelper,
   PerspectiveCamera,
   Scene,
-  WebGLRenderer
+  WebGLRenderer,
+  Texture
 } from 'three';
 import './main.css';
+
+const image = new Image();
+const texture = new Texture(image);
+
+image.onload = () => {
+  // ask ThreeJS to force update the texture
+  texture.needsUpdate = true;
+};
+
+image.src = '/textures/door/color.jpg';
 
 const canvas = document.createElement('canvas');
 canvas.setAttribute('id', 'three-js-stage');
@@ -21,12 +32,14 @@ const dimension = {
 const mesh = new Mesh(
   new BoxGeometry(1, 1, 1),
   new MeshBasicMaterial({
-    color: 0xff0000
+    // color: 0xff0000
+    map: texture
   })
 );
 
 const camera = new PerspectiveCamera(75, dimension.width / dimension.height);
-camera.position.set(0, 0, 3);
+camera.position.set(1.5, 1.5, 1.5);
+camera.lookAt(mesh.position)
 
 const axesHelper = new AxesHelper(1000);
 
@@ -42,3 +55,11 @@ const renderer = new WebGLRenderer({
 renderer.setSize(dimension.width, dimension.height);
 
 renderer.render(scene, camera);
+
+function gameLoop() {
+  renderer.render(scene, camera);
+
+  requestAnimationFrame(gameLoop);
+}
+
+requestAnimationFrame(gameLoop);
