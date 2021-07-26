8e35f2cd6408db6d269241ecec1b0b384cae9a83
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2021-07-26 22:00:21 +0530

    Text Geometry, transformation on geometry and optimizations
    - Used fort loader to load fonts
    - Created Font Geometry from it
    - Learnt to center fonts with bezels
    - Optimization for material and geomerty

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,30 @@ import {
   AxesHelper,
   PerspectiveCamera,
   Scene,
-  WebGLRenderer
+  WebGLRenderer,
+  FontLoader,
+  TextBufferGeometry,
+  Vector3,
+  MeshMatcapMaterial,
+  TextureLoader,
+  TorusBufferGeometry
 } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+/** Importing fonts directly */
+// import typefaceFronts from 'three/examples/fonts/helvetiker_regular.typeface.json';
 import { GUI } from 'dat.gui';
 import gsap from 'gsap';
 import './main.css';
 
 const gui = new GUI();
+
+// Font loader
+const fontLoader = new FontLoader();
+
+// Texture loader
+const textureLoader = new TextureLoader();
+
+const matcapTexture = textureLoader.load('/matcap/sample.png');
 
 const canvas = document.createElement('canvas');
 canvas.setAttribute('id', 'three-js-stage');
@@ -23,13 +39,6 @@ const dimension = {
   width: window.innerWidth
 };
 
-const mesh = new Mesh(
-  new BoxGeometry(1, 1, 1),
-  new MeshBasicMaterial({
-    color: 0xff0000
-  })
-);
-
 const camera = new PerspectiveCamera(75, dimension.width / dimension.height);
 camera.position.set(0, 0, 3);
 
@@ -37,7 +46,61 @@ const axesHelper = new AxesHelper(1000);
 
 const scene = new Scene();
 
-scene.add(mesh);
+fontLoader.load('/fonts/helvetiker_regular.typeface.json', (font) => {
+  console.log('Font loaded');
+  const textGeometry = new TextBufferGeometry('Aakash Mallik', {
+    font,
+    size: 0.5,
+    height: 0.2,
+    curveSegments: 6, // keep it low
+    bevelEnabled: true,
+    bevelThickness: 0.03,
+    bevelSize: 0.02,
+    bevelOffset: 0,
+    bevelSegments: 4 // keep it low
+  });
+
+  // textGeometry.computeBoundingBox();
+  // textGeometry.translate(
+  //   -(textGeometry.boundingBox.max.x - 0.02) * 0.5, // bevel size needs to be subtracted
+  //   -(textGeometry.boundingBox.max.y - 0.02) * 0.5, // bevel size needs to be subtracted
+  //   -(textGeometry.boundingBox.max.z - 0.03) * 0.5 // bevel thickness needs to be subtracted
+  // );
+
+  textGeometry.center();
+
+  const material = new MeshMatcapMaterial({
+    matcap: matcapTexture
+  });
+  const textMesh = new Mesh(textGeometry, material);
+  scene.add(textMesh);
+
+  console.time('donut');
+
+  const torusGeometry = new TorusBufferGeometry(0.3, 0.2, 20, 45);
+  // const torusMaterial = new MeshMatcapMaterial({ matcap: matcapTexture }); // optimization
+
+  for (let i = 0; i < 100; i++) {
+    const mesh = new Mesh(torusGeometry, material);
+
+    mesh.position.set(
+      10 * (Math.random() - 0.5),
+      10 * (Math.random() - 0.5),
+      10 * (Math.random() - 0.5)
+    );
+
+    mesh.rotateX((Math.random() - 0.5) * Math.PI);
+    mesh.rotateY((Math.random() - 0.5) * Math.PI);
+
+    const scale = Math.random();
+    mesh.scale.set(scale, scale, scale);
+
+    scene.add(mesh);
+  }
+
+  console.timeEnd('donut');
+});
+
 scene.add(camera);
 scene.add(axesHelper);
 
@@ -48,9 +111,7 @@ renderer.setSize(dimension.width, dimension.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 const control = new OrbitControls(camera, canvas);
-control.target.x = mesh.position.x;
-control.target.y = mesh.position.y;
-control.target.z = mesh.position.z;
+control.target = new Vector3(0, 0, 0);
 
 window.addEventListener('resize', function () {
   dimension.height = window.innerHeight;
@@ -62,29 +123,6 @@ window.addEventListener('resize', function () {
   renderer.setSize(dimension.width, dimension.height);
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
-
-const cubeFolder = gui.addFolder('Red Cube');
-cubeFolder.add(mesh.position, 'x').name('position-x').min(-10).max(10).step(0.001);
-cubeFolder.add(mesh.position, 'y').name('position-y').min(-10).max(10).step(0.001);
-cubeFolder.add(mesh.position, 'z').name('position-z').min(-10).max(10).step(0.001);
-cubeFolder
-  .add(mesh.rotation, 'x')
-  .name('rotation-x')
-  .min(-2 * Math.PI)
-  .max(2 * Math.PI)
-  .step(0.001);
-cubeFolder
-  .add(mesh.rotation, 'y')
-  .name('rotation-y')
-  .min(-2 * Math.PI)
-  .max(2 * Math.PI)
-  .step(0.001);
-cubeFolder
-  .add(mesh.rotation, 'z')
-  .name('rotation-z')
-  .min(-2 * Math.PI)
-  .max(2 * Math.PI)
-  .step(0.001);
 
 let currentTimestamp = performance.now();
 let rafId: number;
