065ff0c62fdc59af7acff258f5a20aef8e0a81d5
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2021-07-28 21:20:31 +0530

    Understood shadows
    - How to enable shadows
    - The performance problems of having shadows
    - How are shadows rendered
    - Enabling shadow camera helpers and tweaking values to optimise
    - Baking shadow on textures
    - Baking shadow alternative to have dynamic baked shadows

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,25 @@
 import {
-  BoxGeometry,
+  SphereBufferGeometry,
   Mesh,
   MeshBasicMaterial,
   AxesHelper,
   PerspectiveCamera,
   Scene,
-  WebGLRenderer
+  WebGLRenderer,
+  MeshStandardMaterial,
+  PlaneBufferGeometry,
+  AmbientLight,
+  DirectionalLight,
+  Vector3,
+  CameraHelper,
+  PCFSoftShadowMap,
+  SpotLight,
+  PointLight
 } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GUI } from 'dat.gui';
 import gsap from 'gsap';
 import './main.css';
-
-const gui = new GUI();
 
 const canvas = document.createElement('canvas');
 canvas.setAttribute('id', 'three-js-stage');
@@ -23,34 +30,82 @@ const dimension = {
   width: window.innerWidth
 };
 
-const mesh = new Mesh(
-  new BoxGeometry(1, 1, 1),
-  new MeshBasicMaterial({
-    color: 0xff0000
-  })
-);
-
-const camera = new PerspectiveCamera(75, dimension.width / dimension.height);
-camera.position.set(0, 0, 3);
-
-const axesHelper = new AxesHelper(1000);
-
+const gui = new GUI();
 const scene = new Scene();
 
-scene.add(mesh);
+const ambientLight = new AmbientLight(0xffffff, 0.2);
+scene.add(ambientLight);
+
+const directionalLight = new DirectionalLight(0xffffff, 0.3);
+scene.add(directionalLight);
+directionalLight.castShadow = true;
+directionalLight.shadow.mapSize.height = 1024;
+directionalLight.shadow.mapSize.width = 1024;
+directionalLight.shadow.camera.near = 1;
+directionalLight.shadow.camera.far = 4;
+directionalLight.shadow.camera.top = 2;
+directionalLight.shadow.camera.left = -2;
+directionalLight.shadow.camera.bottom = -2;
+directionalLight.shadow.camera.right = 2;
+directionalLight.shadow.radius = 10;
+// scene.add(new CameraHelper(directionalLight.shadow.camera));
+
+const spotLight = new SpotLight(0xffffff, 0.7, 20, Math.PI / 0.3, 0.2, 2);
+spotLight.position.setY(3);
+spotLight.position.setZ(3);
+spotLight.target.position.set(0, 0, 0);
+spotLight.castShadow = true;
+spotLight.shadow.mapSize.height = 1024;
+spotLight.shadow.mapSize.width = 1024;
+spotLight.shadow.camera.fov = 30;
+spotLight.shadow.camera.far = 8;
+scene.add(spotLight);
+// scene.add(new CameraHelper(spotLight.shadow.camera));
+
+const pointLight = new PointLight(0xffffff, 0.3);
+pointLight.position.set(4, 4, 0);
+pointLight.castShadow = true;
+pointLight.shadow.mapSize.height = 1024;
+pointLight.shadow.mapSize.width = 1024;
+pointLight.shadow.camera.far = 14;
+scene.add(pointLight);
+// scene.add(new CameraHelper(pointLight.shadow.camera));
+
+
+const material = new MeshStandardMaterial({
+  color: 0xffffff,
+  roughness: 0.7
+  // wireframe: true
+});
+
+const sphereMesh = new Mesh(new SphereBufferGeometry(1, 24, 24), material);
+scene.add(sphereMesh);
+sphereMesh.castShadow = true;
+sphereMesh.receiveShadow = false; // nothing on top
+
+const planeMesh = new Mesh(new PlaneBufferGeometry(12, 12), material);
+scene.add(planeMesh);
+planeMesh.rotateX(-Math.PI / 2);
+planeMesh.position.setY(-1.3);
+planeMesh.castShadow = false;
+planeMesh.receiveShadow = true;
+
+const camera = new PerspectiveCamera(75, dimension.width / dimension.height);
 scene.add(camera);
-scene.add(axesHelper);
+camera.position.set(1, 1, 3);
+
+// scene.add(new AxesHelper(1000));
 
 const renderer = new WebGLRenderer({
   canvas
 });
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = PCFSoftShadowMap;
 renderer.setSize(dimension.width, dimension.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 const control = new OrbitControls(camera, canvas);
-control.target.x = mesh.position.x;
-control.target.y = mesh.position.y;
-control.target.z = mesh.position.z;
+control.target = new Vector3();
 
 window.addEventListener('resize', function () {
   dimension.height = window.innerHeight;
@@ -63,27 +118,31 @@ window.addEventListener('resize', function () {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
-const cubeFolder = gui.addFolder('Red Cube');
-cubeFolder.add(mesh.position, 'x').name('position-x').min(-10).max(10).step(0.001);
-cubeFolder.add(mesh.position, 'y').name('position-y').min(-10).max(10).step(0.001);
-cubeFolder.add(mesh.position, 'z').name('position-z').min(-10).max(10).step(0.001);
-cubeFolder
-  .add(mesh.rotation, 'x')
-  .name('rotation-x')
-  .min(-2 * Math.PI)
-  .max(2 * Math.PI)
+gui.add(ambientLight, 'intensity').name('ambient light intensity').min(0).max(1).step(0.0001);
+const directionalLightFolder = gui.addFolder('Directional Light');
+directionalLightFolder
+  .add(directionalLight, 'intensity')
+  .name('directional light intensity')
+  .min(0)
+  .max(1)
+  .step(0.0001);
+directionalLightFolder
+  .add(directionalLight.position, 'x')
+  .name('light-x')
+  .min(-10)
+  .max(10)
   .step(0.001);
-cubeFolder
-  .add(mesh.rotation, 'y')
-  .name('rotation-y')
-  .min(-2 * Math.PI)
-  .max(2 * Math.PI)
+directionalLightFolder
+  .add(directionalLight.position, 'y')
+  .name('light-y')
+  .min(-10)
+  .max(10)
   .step(0.001);
-cubeFolder
-  .add(mesh.rotation, 'z')
-  .name('rotation-z')
-  .min(-2 * Math.PI)
-  .max(2 * Math.PI)
+directionalLightFolder
+  .add(directionalLight.position, 'z')
+  .name('light-z')
+  .min(-10)
+  .max(10)
   .step(0.001);
 
 let currentTimestamp = performance.now();
