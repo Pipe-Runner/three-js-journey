4e64b08e57de324b984cd8626378530e9f51004c
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2021-08-08 00:34:44 +0530

    Implementing a raycaster
    - Implemented a static ray caster in the scene
    - Implemented a ray caster moving with the mouse and camera
    - Implemented mouse enter and leave events by making use of a witness
    variable
    - Extended it further by implementing a click event

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,11 @@ import {
   AxesHelper,
   PerspectiveCamera,
   Scene,
-  WebGLRenderer
+  WebGLRenderer,
+  Vector3,
+  SphereBufferGeometry,
+  Raycaster,
+  Vector2
 } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GUI } from 'dat.gui';
@@ -13,6 +17,7 @@ import gsap from 'gsap';
 import './main.css';
 
 const gui = new GUI();
+const scene = new Scene();
 
 const canvas = document.createElement('canvas');
 canvas.setAttribute('id', 'three-js-stage');
@@ -23,21 +28,48 @@ const dimension = {
   width: window.innerWidth
 };
 
-const mesh = new Mesh(
-  new BoxGeometry(1, 1, 1),
+const sphere1 = new Mesh(
+  new SphereBufferGeometry(1, 32, 32),
   new MeshBasicMaterial({
     color: 0xff0000
   })
 );
+scene.add(sphere1);
+sphere1.position.setX(-5);
+
+const sphere2 = new Mesh(
+  new SphereBufferGeometry(1, 32, 32),
+  new MeshBasicMaterial({
+    color: 0xff0000
+  })
+);
+scene.add(sphere2);
+
+const sphere3 = new Mesh(
+  new SphereBufferGeometry(1, 32, 32),
+  new MeshBasicMaterial({
+    color: 0xff0000
+  })
+);
+scene.add(sphere3);
+sphere3.position.setX(5);
+
+/**
+ * Raycaster
+ */
+
+const raycaster = new Raycaster(new Vector3(-10, 0, 0), new Vector3(10, 0, 0).normalize());
+
+// const intersect = raycaster.intersectObject(sphere1);
+// const intersects = raycaster.intersectObjects([sphere1, sphere2, sphere3]);
+
+// console.log(intersect, intersects);
 
 const camera = new PerspectiveCamera(75, dimension.width / dimension.height);
-camera.position.set(0, 0, 3);
+camera.position.set(0, 0, 6);
 
 const axesHelper = new AxesHelper(1000);
 
-const scene = new Scene();
-
-scene.add(mesh);
 scene.add(camera);
 scene.add(axesHelper);
 
@@ -48,9 +80,7 @@ renderer.setSize(dimension.width, dimension.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 const control = new OrbitControls(camera, canvas);
-control.target.x = mesh.position.x;
-control.target.y = mesh.position.y;
-control.target.z = mesh.position.z;
+control.target = new Vector3();
 
 window.addEventListener('resize', function () {
   dimension.height = window.innerHeight;
@@ -63,33 +93,34 @@ window.addEventListener('resize', function () {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
-const cubeFolder = gui.addFolder('Red Cube');
-cubeFolder.add(mesh.position, 'x').name('position-x').min(-10).max(10).step(0.001);
-cubeFolder.add(mesh.position, 'y').name('position-y').min(-10).max(10).step(0.001);
-cubeFolder.add(mesh.position, 'z').name('position-z').min(-10).max(10).step(0.001);
-cubeFolder
-  .add(mesh.rotation, 'x')
-  .name('rotation-x')
-  .min(-2 * Math.PI)
-  .max(2 * Math.PI)
-  .step(0.001);
-cubeFolder
-  .add(mesh.rotation, 'y')
-  .name('rotation-y')
-  .min(-2 * Math.PI)
-  .max(2 * Math.PI)
-  .step(0.001);
-cubeFolder
-  .add(mesh.rotation, 'z')
-  .name('rotation-z')
-  .min(-2 * Math.PI)
-  .max(2 * Math.PI)
-  .step(0.001);
+const mouse = new Vector2();
+canvas.addEventListener('mousemove', (event: MouseEvent) => {
+  const verticalPosition = (event.offsetY / dimension.height) * 2 - 1;
+  const horizontalPosition = (event.offsetX / dimension.width) * 2 - 1;
+
+  mouse.setX(horizontalPosition);
+  mouse.setY(-verticalPosition);
+});
 
 let currentTimestamp = performance.now();
 let rafId: number;
 
+const objectList = [sphere3, sphere2, sphere1];
+
 function gameLoop(timestamp) {
+  sphere1.position.setY(3 * Math.sin((100 * sphere1.position.x + timestamp) * 0.001));
+  sphere2.position.setY(3 * Math.sin(timestamp * 0.001));
+  sphere3.position.setY(3 * Math.sin((100 * sphere3.position.x + timestamp) * 0.001));
+
+  raycaster.setFromCamera(mouse, camera); // using raycaster from camera
+
+  const intersectingArray = raycaster.intersectObjects(objectList);
+  objectList.forEach((object) => object.material.color.set('#ff0000'));
+  intersectingArray.forEach((intersectingItem) => {
+    const object = intersectingItem.object as Mesh<SphereBufferGeometry, MeshBasicMaterial>;
+    object.material.color.set('#ff00ff');
+  });
+
   control.update();
   renderer.render(scene, camera);
   rafId = requestAnimationFrame(gameLoop);
