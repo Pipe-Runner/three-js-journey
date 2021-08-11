c44c96cb78fc6fa1d236299fbe1246213436f0fd
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2021-08-12 01:41:47 +0530

    Played around with Physis using CannonJS
    - Physis world using cannon JS
    - 3D world using ThreeJS
    - Both will be coupled together

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,73 @@ import {
   AxesHelper,
   PerspectiveCamera,
   Scene,
-  WebGLRenderer
+  WebGLRenderer,
+  Vector3,
+  SphereBufferGeometry,
+  PlaneBufferGeometry,
+  AmbientLight,
+  MeshStandardMaterial,
+  DirectionalLight,
+  CameraHelper,
+  Clock
 } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GUI } from 'dat.gui';
+import { Body, ContactMaterial, Material, Plane, Sphere, Vec3, World } from 'cannon';
 import gsap from 'gsap';
 import './main.css';
 
+/**
+ * CANNON
+ */
+
+const world = new World();
+
+/**
+ * Material
+ */
+const concreteMaterial = new Material('concrete');
+const plasticMaterial = new Material('plastic');
+
+/**
+ * Contact Material
+ */
+const concreteVsPlasticMaterial = new ContactMaterial(concreteMaterial, plasticMaterial, {
+  friction: 0.1,
+  restitution: 0.7
+});
+world.addContactMaterial(concreteVsPlasticMaterial);
+
+world.gravity.set(0, -9.82, 0);
+
+/**
+ * Bodies
+ */
+const sphereShape = new Sphere(0.5);
+const sphereBody = new Body({
+  mass: 1,
+  position: new Vec3(0, 5, 0),
+  shape: sphereShape,
+  material: plasticMaterial
+});
+sphereBody.applyLocalForce(new Vec3(0, 0, -150), new Vec3(0, 0, 0));
+world.addBody(sphereBody);
+
+const planeShape = new Plane();
+const planeBody = new Body({
+  mass: 0,
+  position: new Vec3(0, 0, 0),
+  shape: planeShape,
+  material: concreteMaterial
+});
+planeBody.quaternion.setFromAxisAngle(new Vec3(1, 0, 0), -Math.PI / 2);
+world.addBody(planeBody);
+
+/**
+ * THREE JS
+ */
 const gui = new GUI();
+const scene = new Scene();
 
 const canvas = document.createElement('canvas');
 canvas.setAttribute('id', 'three-js-stage');
@@ -23,34 +82,63 @@ const dimension = {
   width: window.innerWidth
 };
 
-const mesh = new Mesh(
-  new BoxGeometry(1, 1, 1),
-  new MeshBasicMaterial({
-    color: 0xff0000
+const ambientLight = new AmbientLight('white', 0.5);
+scene.add(ambientLight);
+
+const directionalLight = new DirectionalLight('white', 0.5);
+directionalLight.position.setX(2.5);
+directionalLight.position.setY(9.5);
+directionalLight.castShadow = true;
+directionalLight.shadow.camera.far = 12;
+directionalLight.shadow.mapSize.width = 512;
+directionalLight.shadow.mapSize.height = 512;
+scene.add(directionalLight);
+// scene.add(new CameraHelper(directionalLight.shadow.camera));
+
+const sphere = new Mesh(
+  new SphereBufferGeometry(0.5, 32, 32),
+  new MeshStandardMaterial({
+    // wireframe: true,
+    color: 'pink',
+    metalness: 0.9,
+    roughness: 0.8
   })
 );
+// sphere.position.setY(3);
+sphere.castShadow = true;
+scene.add(sphere);
+
+const plane = new Mesh(
+  new PlaneBufferGeometry(8, 8),
+  new MeshStandardMaterial({
+    // wireframe: true,
+    color: 'white',
+    metalness: 0.5,
+    roughness: 0.8
+  })
+);
+plane.receiveShadow = true;
+plane.rotateX(-Math.PI / 2);
+scene.add(plane);
 
 const camera = new PerspectiveCamera(75, dimension.width / dimension.height);
-camera.position.set(0, 0, 3);
+camera.position.set(3, 3, 3);
 
 const axesHelper = new AxesHelper(1000);
 
-const scene = new Scene();
-
-scene.add(mesh);
+// scene.add(mesh);
 scene.add(camera);
 scene.add(axesHelper);
 
 const renderer = new WebGLRenderer({
   canvas
 });
+renderer.shadowMap.enabled = true;
 renderer.setSize(dimension.width, dimension.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 const control = new OrbitControls(camera, canvas);
-control.target.x = mesh.position.x;
-control.target.y = mesh.position.y;
-control.target.z = mesh.position.z;
+control.target = new Vector3();
 
 window.addEventListener('resize', function () {
   dimension.height = window.innerHeight;
@@ -63,36 +151,33 @@ window.addEventListener('resize', function () {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
-const cubeFolder = gui.addFolder('Red Cube');
-cubeFolder.add(mesh.position, 'x').name('position-x').min(-10).max(10).step(0.001);
-cubeFolder.add(mesh.position, 'y').name('position-y').min(-10).max(10).step(0.001);
-cubeFolder.add(mesh.position, 'z').name('position-z').min(-10).max(10).step(0.001);
-cubeFolder
-  .add(mesh.rotation, 'x')
-  .name('rotation-x')
-  .min(-2 * Math.PI)
-  .max(2 * Math.PI)
-  .step(0.001);
-cubeFolder
-  .add(mesh.rotation, 'y')
-  .name('rotation-y')
-  .min(-2 * Math.PI)
-  .max(2 * Math.PI)
-  .step(0.001);
-cubeFolder
-  .add(mesh.rotation, 'z')
-  .name('rotation-z')
-  .min(-2 * Math.PI)
-  .max(2 * Math.PI)
-  .step(0.001);
+const directionLightFolder = gui.addFolder('Directional Light');
+directionLightFolder.add(directionalLight.position, 'x').min(0).max(100).step(0.5);
+directionLightFolder.add(directionalLight.position, 'y').min(0).max(100).step(0.5);
+directionLightFolder.add(directionalLight.position, 'z').min(0).max(100).step(0.5);
 
-let currentTimestamp = performance.now();
+let oldElapsedTime = 0;
+const clock = new Clock();
 let rafId: number;
 
-function gameLoop(timestamp) {
+function gameLoop() {
+  const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - oldElapsedTime;
+  oldElapsedTime = elapsedTime;
+
   control.update();
   renderer.render(scene, camera);
+
+  world.step(1 / 60, deltaTime, 3);
+
+  /**
+   * Update sphere position
+   */
+  sphere.position.setX(sphereBody.position.x);
+  sphere.position.setY(sphereBody.position.y);
+  sphere.position.setZ(sphereBody.position.z);
+
   rafId = requestAnimationFrame(gameLoop);
 }
 
-rafId = requestAnimationFrame(gameLoop);
+gameLoop();
