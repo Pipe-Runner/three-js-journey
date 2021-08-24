81e34023e7acb8401bedd7f476baefadd8d9544b
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2021-08-25 01:15:45 +0530

    Learned about realistic renders
    - Output encoding
    - Texture encoding
    - Tone Mapping
    - Anti-aliasing
    - Shadow acne / Shadow bias or normal bias

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,168 @@
 import {
-  BoxGeometry,
-  Mesh,
-  MeshBasicMaterial,
   AxesHelper,
   PerspectiveCamera,
   Scene,
-  WebGLRenderer
+  WebGLRenderer,
+  AmbientLight,
+  DirectionalLight,
+  Vector3,
+  CubeTextureLoader,
+  Mesh,
+  MeshStandardMaterial,
+  sRGBEncoding,
+  CineonToneMapping,
+  NoToneMapping,
+  ReinhardToneMapping,
+  LinearToneMapping,
+  ACESFilmicToneMapping,
+  PCFShadowMap,
+  CameraHelper,
 } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { GUI } from 'dat.gui';
 import gsap from 'gsap';
 import './main.css';
-
-const gui = new GUI();
-
-const canvas = document.createElement('canvas');
-canvas.setAttribute('id', 'three-js-stage');
-document.body.appendChild(canvas);
 
 const dimension = {
   height: window.innerHeight,
   width: window.innerWidth
 };
 
-const mesh = new Mesh(
-  new BoxGeometry(1, 1, 1),
-  new MeshBasicMaterial({
-    color: 0xff0000
-  })
-);
+/**
+ * Getting canvas instance
+ */
+const canvas = document.createElement('canvas');
+canvas.setAttribute('id', 'three-js-stage');
+document.body.appendChild(canvas);
 
-const camera = new PerspectiveCamera(75, dimension.width / dimension.height);
-camera.position.set(0, 0, 3);
-
-const axesHelper = new AxesHelper(1000);
-
+const gui = new GUI();
 const scene = new Scene();
 
-scene.add(mesh);
+const debugObject = {
+  envMapIntensity: 1.3
+}
+
+gui.add(debugObject, 'envMapIntensity').min(0).max(20).step(0.01).onChange(() => {
+  scene.traverse((child) => {
+    if( child instanceof Mesh && child.material instanceof MeshStandardMaterial ){
+      child.material.envMapIntensity = debugObject.envMapIntensity;
+    }
+  })
+});
+
+/**
+ * object traversal function
+ */
+function updateEnvMapOnAll(envMap){
+  scene.traverse((child) => {
+    if( child instanceof Mesh && child.material instanceof MeshStandardMaterial ){
+      child.receiveShadow = true;
+      child.castShadow = true;
+      child.material.envMap = envMap;
+      child.material.envMapIntensity = debugObject.envMapIntensity;
+    }
+  })
+}
+
+/**
+ * Model loader
+ */
+ const gltfLoader = new GLTFLoader();
+ 
+ /**
+  * Environment map loader
+  */
+ const cubeTextureLoader = new CubeTextureLoader();
+ cubeTextureLoader.load(['/textures/environmentMaps/0/px.jpg','/textures/environmentMaps/0/nx.jpg','/textures/environmentMaps/0/py.jpg','/textures/environmentMaps/0/ny.jpg','/textures/environmentMaps/0/pz.jpg','/textures/environmentMaps/0/nz.jpg'], (environmentMap) => {
+   environmentMap.encoding = sRGBEncoding;
+   scene.background = environmentMap;
+
+   gltfLoader.load('/models/FlightHelmet/glTF/FlightHelmet.gltf', (flightHelmetGltf) => {
+    const helmetGroup = flightHelmetGltf.scene;
+    helmetGroup.scale.set(3, 3, 3);
+    scene.add(helmetGroup);
+
+    //applying env map on all objects in the scene
+    updateEnvMapOnAll(environmentMap);
+  })
+ })
+
+/**
+ * Adding lights to the scene (AL)
+ */
+const ambientLight = new AmbientLight('white', 0.25);
+scene.add(ambientLight);
+
+/**
+ * Adding lights to the scene (DL)
+ */
+const directionalLight = new DirectionalLight('white', 2.7);
+directionalLight.position.set(0.25, 3, -2.25);
+directionalLight.shadow.camera.far = 4.3;
+directionalLight.castShadow = true;
+directionalLight.shadow.mapSize.set(1024, 1024);
+scene.add(directionalLight);
+// NOTE: Try shadow bias / shadow normal bias if you get shadow acne
+
+const guiDirectionalLight = gui.addFolder('directional-light');
+guiDirectionalLight.add(directionalLight.position, 'x').min(-10).max(10).step(0.01);
+guiDirectionalLight.add(directionalLight.position, 'y').min(-10).max(10).step(0.01);
+guiDirectionalLight.add(directionalLight.position, 'z').min(-10).max(10).step(0.01);
+guiDirectionalLight.add(directionalLight, 'intensity').min(0).max(10).step(0.001);
+
+const directionLightCameraHelper = new CameraHelper(directionalLight.shadow.camera);
+scene.add(directionLightCameraHelper);
+
+/**
+ * Camera
+ */
+const camera = new PerspectiveCamera(75, dimension.width / dimension.height);
+camera.position.set(3, 3, 3);
 scene.add(camera);
+
+/**
+ * Axis helper
+ */
+const axesHelper = new AxesHelper(1000);
 scene.add(axesHelper);
 
 const renderer = new WebGLRenderer({
-  canvas
+  canvas,
+  antialias: true
 });
+renderer.physicallyCorrectLights = true;
+renderer.outputEncoding = sRGBEncoding;
+renderer.toneMapping = CineonToneMapping;
+renderer.toneMappingExposure = 2;
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = PCFShadowMap;
 renderer.setSize(dimension.width, dimension.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
+gui.add(renderer, 'toneMapping', {
+  "None" : NoToneMapping,
+  "Cineon" : CineonToneMapping,
+  "Reinhard": ReinhardToneMapping,
+  "Linear": LinearToneMapping,
+  "ACESFilmic": ACESFilmicToneMapping
+}).onFinishChange(() => {
+  renderer.toneMapping = Number(renderer.toneMapping);
+
+  scene.traverse((child) => {
+    if( child instanceof Mesh && child.material instanceof MeshStandardMaterial ){
+      child.material.needsUpdate = true;
+    }
+  })
+})
+
+gui.add(renderer, 'toneMappingExposure').min(0).max(5).step(0.1);
+
+/**
+ * Orbit controls
+ */
 const control = new OrbitControls(camera, canvas);
-control.target.x = mesh.position.x;
-control.target.y = mesh.position.y;
-control.target.z = mesh.position.z;
+control.target = new Vector3();
 
 window.addEventListener('resize', function () {
   dimension.height = window.innerHeight;
@@ -62,29 +174,6 @@ window.addEventListener('resize', function () {
   renderer.setSize(dimension.width, dimension.height);
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
-
-const cubeFolder = gui.addFolder('Red Cube');
-cubeFolder.add(mesh.position, 'x').name('position-x').min(-10).max(10).step(0.001);
-cubeFolder.add(mesh.position, 'y').name('position-y').min(-10).max(10).step(0.001);
-cubeFolder.add(mesh.position, 'z').name('position-z').min(-10).max(10).step(0.001);
-cubeFolder
-  .add(mesh.rotation, 'x')
-  .name('rotation-x')
-  .min(-2 * Math.PI)
-  .max(2 * Math.PI)
-  .step(0.001);
-cubeFolder
-  .add(mesh.rotation, 'y')
-  .name('rotation-y')
-  .min(-2 * Math.PI)
-  .max(2 * Math.PI)
-  .step(0.001);
-cubeFolder
-  .add(mesh.rotation, 'z')
-  .name('rotation-z')
-  .min(-2 * Math.PI)
-  .max(2 * Math.PI)
-  .step(0.001);
 
 let currentTimestamp = performance.now();
 let rafId: number;
